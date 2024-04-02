b6adb5c79d0403b88abf8517ac50dbbaeae67f72
Jessi Pavia <jessi316866@gmail.com>
2024-04-02 15:48:57 -0400

    javascript assignment

---
+++ js/script.js
@@ -0,0 +1,28 @@
+console.log('My name is Jessi');
+
+console.log(`
+    Hello
+    World!
+`);
+
+var message = "Welcome!";
+let user1 = "Max";
+let user2 = "Anna";
+const username = "christ11";
+
+console.log(message);
+console.log(user1);
+
+let userInput;
+userInput = 'Jessi';
+userInput = prompt('enter your name: ');
+console.log(userInput);
+
+const userId = 'idx8548jjer';
+console.log(userId);
+
+
+let postViews = 0;
+postViews = 1;
+console.log(postViews);
+
